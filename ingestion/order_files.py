6ccc7f1dd825e9c7994eb3a09d351339380c66af
Max Gustafson <maxgustafson97@gmail.com>
2026-08-23 19:47:05 +0200

    ingestion revisited

--- ingestion/order_files.py
+++ ingestion/order_files.py
@@ -8,34 +8,51 @@ ASSUMPTIONS:
 
 TODO:
     Proper orchestration tool instead of if __name__ == __main__
-    Incremental load
     Dump in local MinIO
+    Move processed files to backup/delete processed files
 
 """
+
+from pathlib import Path
 
 from tools.file_resourses import file_resource_pipeline_csv
 
 _DOMAIN: str = "orders"
+_DATABASE_PATH: str = "databases/orders.duckdb"
 
 
-def customers() -> None:
-    file_resource_pipeline_csv(table="customers", domain=_DOMAIN)
+def dynamic_order_files_resource_factory():
 
+    allow_listed_files: set = {"customers", "order_items", "orders", "products"}
 
-def orders() -> None:
-    file_resource_pipeline_csv(table="orders", domain=_DOMAIN)
+    input_folder: Path = Path("input_data")
+    errors: list[str] = []
 
+    for csv_file in input_folder.glob("*.csv"):
+        try:
+            if csv_file.stem not in allow_listed_files:
+                raise ValueError(
+                    f"Blocked file {csv_file.name} processed for ingestion."
+                )
+            else:
+                file_resource_pipeline_csv(
+                    table=csv_file.stem,
+                    domain=_DOMAIN,
+                    trg_destination_path=_DATABASE_PATH,
+                    src_filesystem_path=input_folder,
+                )
 
-def order_items() -> None:
-    file_resource_pipeline_csv(table="order_items", domain=_DOMAIN)
+        except ValueError as e:
+            errors.append(str(e))
 
-
-def products() -> None:
-    file_resource_pipeline_csv(table="products", domain=_DOMAIN)
+    # Error handling
+    if errors:
+        raise RuntimeError(
+            f"{len(errors)} file(s) could not be processed:\n"
+            + "\n".join(f"- {error}" for error in errors)
+            + f"\nThe following file-stems are allowed : {[stem + '.csv' for stem in allow_listed_files]}. "
+        )
 
 
 if __name__ == "__main__":
-    customers()
-    orders()
-    order_items()
-    products()
+    dynamic_order_files_resource_factory()
