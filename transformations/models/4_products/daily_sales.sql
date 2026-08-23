e02b33e6836169c7a72894368eac2bf096ae683c
Max Gustafson <maxgustafson97@gmail.com>
2026-08-23 19:44:31 +0200

    feat: Initial Commit Product Layer

---
+++ transformations/models/4_products/daily_sales.sql
@@ -0,0 +1,26 @@
+with orders_aggregated as (
+
+    select
+        order_id,
+        any_value(order_timestamp) as order_timestamp,
+        sum(line_amount) as order_total_amount
+    from {{ ref('fct_order_items') }}
+    group by order_id
+),
+
+date_aggregation as (
+    select
+        cast(order_timestamp as date) as order_date,
+        sum(order_total_amount) as total_revenue,
+        count(order_id) as total_orders,
+        avg(order_total_amount) as average_order_value
+    from orders_aggregated
+    group by cast(order_timestamp as date)
+)
+
+select
+    order_date,
+    total_revenue,
+    total_orders,
+    average_order_value
+from date_aggregation
