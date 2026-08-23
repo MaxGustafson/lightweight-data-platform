2fd799e74254d0b3d364b4c6401f294e521e58a1
Max Gustafson <maxgustafson97@gmail.com>
2026-08-23 13:20:09 +0200

    feat: Core models
    Inital Core models

--- transformations/models/3_core/fct_order_items.sql
+++ transformations/models/3_core/fct_order_items.sql
@@ -1,1 +1,15 @@
-select * from {{ ref('int_order_items') }}
+select
+    order_item_guid,
+    order_guid,
+    customer_guid,
+    product_guid,
+    order_id,
+    product_id,
+    customer_id,
+    order_timestamp,
+    order_status,
+    quantity,
+    price_per_unit,
+    source_system,
+    line_amount
+from {{ ref('int_order_items') }}
